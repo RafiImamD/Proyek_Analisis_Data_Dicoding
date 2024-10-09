ccb2cff8a332dbd9d0686fb81e73bada024c20a2
RafiImamD <152243277+RafiImamD@users.noreply.github.com>
2024-10-10 06:54:15 +0700

    Update dashboard_sederhana.py
    Signed-off-by: RafiImamD <152243277+RafiImamD@users.noreply.github.com>

--- dashboard/dashboard_sederhana.py
+++ dashboard/dashboard_sederhana.py
@@ -6,7 +6,7 @@ import streamlit as st
 from babel.numbers import format_currency
 
 # Import dataset lengkap_df
-lengkap_df = pd.read_csv("dashboard/lengkap_df.csv")
+lengkap_df = pd.read_csv("lengkap_df.csv")
 
 st.title('Olist E-Commerce Public Data Analysis')
 
